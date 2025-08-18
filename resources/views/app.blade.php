c7a660e76a73105431ba1fcd737ce4b445ea8846
Angel Gómez <uvangel.gomez@gmail.com>
2025-08-18 08:51:57 -0600

    icono correcto para PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -55,7 +55,7 @@
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
